85db5d1598095edef0d59388c1e793937a001480
Pavitra <pavitralalwani91@gmail.com>
2023-08-15 00:29:55 +0530

    Initial Commit

---
+++ crm/dcrm/mydb.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = '12345678'
+)
+
+cursorObject = dataBase.cursor()
+cursorObject.execute('CREATE DATABASE pavitra')
+print('All done!')
